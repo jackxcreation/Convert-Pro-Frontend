db9a9321d978b66a34afdce0b1a5869d40d3e56d
jackxcreation <jodjack64@gmail.com>
2025-09-08 21:22:39 +0530

    fix: remove experimental.appDir from next.config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** next.config.js */
+const nextConfig = {
+  reactStrictMode: true,
+  // Remove any experimental.appDir entries
+  // If you need other config, add here (but don't set experimental.appDir)
+};
+
+module.exports = nextConfig;
